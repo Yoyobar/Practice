f85c85fc3fbb0cf45749e2140f92f8b6f6f0cd01
ybar2615 <barwait@naver.com>
2022-12-09 00:04:21 +0900

    22/12/08

--- Java/src/study/chapter/six/Mondai_06_3_2.java
+++ Java/src/study/chapter/six/Mondai_06_3_2.java
@@ -3,29 +3,16 @@ package study.chapter.six;
 public class Mondai_06_3_2 {
     //인자로 정수 n을 전달받아서 이진수를 계산하여 반환하는 메소드를 재귀의 형태로 정의하고 이를 호출하는 main 메소드를 정의하자.
     public static void main(String[] args) {
-        toBinary(10);
+        dataR(10);
     }
-
-    public static int toBinary(int n) {
+    public static int dataR(int n){
         if (n > 0) {
-            int mem = n / 2;
-            int bin = n % 2;
+            int mem = n / 2; //5
+            int data = n % 2;  //0
             n /= 2;
 
-            toBinary(n);
-            System.out.print(bin);
-        }
-        return 0;
-    }
-
-    public static int toTest(int n) {
-        if (n > 0) {
-            int value = n / 2;
-            int number = n % 2;
-            n /= 2;
-
-            toTest(n);
-            System.out.print(number);
-        } return 0;
+            dataR(n);
+            System.out.print(data);
+        }return 0;
     }
 }
