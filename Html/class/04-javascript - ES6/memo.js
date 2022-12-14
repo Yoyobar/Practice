0a010cb1aa4851b400b05e84557b48cf8ee72179
ybar2615 <barwait@naver.com>
2022-12-14 23:54:00 +0900

    22/12/14

---
+++ Html/class/04-javascript - ES6/memo.js
@@ -0,0 +1,11 @@
+/**
+
+Jquery 사용법
+
+$ = document.getElementsByClassName 
+.css('', '') = .style.___
+.html = 
+.innerHtml = .html()
+.on = addEventListenr (click, scroll, keydown등)
+.fadeOut, slideUp()등 애니메이션 탑재
+*/
