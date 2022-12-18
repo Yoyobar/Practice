9404cbbf5655275d7c9e498d0877cf74ba84979e
ybar2615 <barwait@naver.com>
2022-12-18 21:49:03 +0900

    22/12/18

--- Html/class/04-javascript - ES6/Practice.js
+++ Html/class/04-javascript - ES6/Practice.js
@@ -17,16 +17,17 @@ var eg = "kim"
 // 그래서 민준이는
 // (1) 변수에 예금액을 넣으면
 // (2) 2년 후의 총 예금액을 자동으로 콘솔창에 출력해주는 기능을 자바스크립트로 만들려고하는데
-// 어떻게 코드를 짜면 될까요? 
-const money = (cost) => {
-    let cost1 = 0.2
-    let cost2 = 0.15
-    if (cost > 50000){
-        console.log(cost+ cost*((cost1*1)*2))
-    }
-    else console.log(cost+ cost*((cost2*1)*2))
+// 어떻게 코드를 짜면 될까요?
+
+const money = (value) => {
+    let variable;
+    if (value >= 50000) {
+        variable = 0.20;
+    }  else variable = 0.15;
+    value = value + (value * variable); 
+    console.log(value);   
 }
-money(60000)
+
 // VM6359:5 84000
 
 // 방금 마신 커피의 3분의 2만큼 총 2번 리필해주는 카페가 있습니다.
@@ -36,7 +37,7 @@ money(60000)
 const coffee = (ml) => {
     let value = (ml / 3 + ml / 3)
     let value2 = (value / 3 + value / 3)
-    console.log("일반" +Math.floor((ml + value + value2)))
+    console.log("일반" + Math.floor((ml + value + value2)))
 }
 
 
@@ -65,3 +66,45 @@ coffee2(100);
 //     } else attempt++;
 // }));
 
+/a/.test('a')
+// true
+/a/.test('abcde')
+// true
+/[a-z]/.test('14145125l')
+// true
+/[A-Z]/.test('14145125Z')
+// true
+/[ㄱ-ㅎ가-힣]/.test('14145125Zㅂ')
+// true
+/[ㄱ-ㅎ가-힣ㅏ-ㅣ]/.test('14145125Zㅂ')
+// true
+/[ㄱ-ㅎ가-힣ㅏ-ㅣ]/.test('14145125Zㅏ')
+// true
+/\S/.test("%$")
+// true
+/\S+@\S+\.\S+/.test("barwait@naver.com")
+// ^시작 $끝 | or & and + 몇번확인하는지
+
+//.toFixed 소수를 반올림, 단 문자열로 변경
+//parseFloat() 문자열 -> 숫자열
+
+/*
+Q1. 함수에 분과 초를 차례로 파라미터로 입력하면 ms단위로 바꿔서 뱉어주는 함수를 만들어봅시다.
+(1초 == 1000ms 입니다)
+*/
+
+const ms = (MIN, SEC) => {
+    let count = (MIN*60+SEC) * 1000;
+    if(MIN > 0){
+        console.log(`${MIN}분 : ${SEC}초 == ${count}ms 입니다.`)
+    } else console.log(`${SEC}초 == ${count}ms 입니다.`)
+}
+/*
+Q2. 가격을 파라미터로 입력하면 10% 할인가를 뱉는 함수를 만들어봅시다.
+근데 첫 구매여부도 true/false로 둘째파라미터에 입력해서 첫 구매가 맞을 경우 추가로 1.5 달러도 할인해줘야합니다. 
+*/
+const cost = (parameter, first) => {
+    if(first === true) {
+        return parameter * 0.9 - 1.5
+    } else return parameter * 0.9
+}
